d2713a10aaf6a497395a419f03dbecd78a965d1e
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2023-02-22 15:56:58 +0100

    feat(MacOS): add support for MacOS binaries (#2)
    ADM64 & ARM64
    
    Relates to https://bonitasoft.atlassian.net/browse/DEV-90

--- build-all-os.sh
+++ build-all-os.sh
@@ -17,3 +17,13 @@ if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for Linux! Aborting the script execution...'
     exit 1
 fi
+env GOOS=darwin GOARCH=amd64 go build -o dist/${package}-amd64-darwin
+if [ $? -ne 0 ]; then
+    echo 'An error has occurred building binary for MacOS amd64! Aborting the script execution...'
+    exit 1
+fi
+env GOOS=darwin GOARCH=arm64 go build -o dist/${package}-arm64-darwin
+if [ $? -ne 0 ]; then
+    echo 'An error has occurred building binary for MacOS arm64! Aborting the script execution...'
+    exit 1
+fi
