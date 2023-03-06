d3eefcdcd2687e8dbf2b113712d4a45942434470
Adrien Kantcheff <adrien.kantcheff@bonitasoft.com>
2023-03-06 14:54:28 +0100

    feat(release): add github action to create release

--- build-all-os.sh
+++ build-all-os.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-package=bonita-packager
-
-env GOOS=windows GOARCH=amd64 go build -o dist/${package}-amd64.exe
+echo 'Building binary for Windows...'
+env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for Windows! Aborting the script execution...'
     exit 1
 fi
-env GOOS=linux GOARCH=amd64 go build -o dist/${package}-linux
+echo 'Done!'
+
+echo 'Building binary for Linux...'
+env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for Linux! Aborting the script execution...'
     exit 1
 fi
-env GOOS=darwin GOARCH=amd64 go build -o dist/${package}-amd64-darwin
+echo 'Done!'
+
+echo 'Building binary for MacOS amd64...'
+env GOOS=darwin GOARCH=amd64 go build -o dist/darwin-amd64/
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for MacOS amd64! Aborting the script execution...'
     exit 1
 fi
-env GOOS=darwin GOARCH=arm64 go build -o dist/${package}-arm64-darwin
+echo 'Done!'
+
+echo 'Building binary for MacOS arm64...'
+env GOOS=darwin GOARCH=arm64 go build -o dist/darwin-arm64/
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for MacOS arm64! Aborting the script execution...'
     exit 1
 fi
+echo 'Done!'
