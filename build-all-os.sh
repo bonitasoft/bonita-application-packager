7fef3812513645642dec8002edc7da3db2a9e280
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2023-03-07 15:42:57 +0100

    fix(bugs): fix CGLIB dep issue on Linux + fix bug on imageBuild only if verbose

--- build-all-os.sh
+++ build-all-os.sh
@@ -3,28 +3,28 @@
 version="${1:-development}"
 
 echo "Building binary ${version} for Windows..."
-env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/ -ldflags="-X 'main.Version=${version}'"
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o dist/windows-amd64/ -ldflags="-X 'main.Version=${version}'"
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for Windows! Aborting the script execution...'
     exit 1
 fi
 
 echo "Building binary ${version} for Linux..."
-env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/ -ldflags="-X 'main.Version=${version}'"
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o dist/linux-amd64/ -ldflags="-X 'main.Version=${version}'"
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for Linux! Aborting the script execution...'
     exit 1
 fi
 
 echo "Building binary ${version} for MacOS amd64..."
-env GOOS=darwin GOARCH=amd64 go build -o dist/darwin-amd64/ -ldflags="-X 'main.Version=${version}'"
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o dist/darwin-amd64/ -ldflags="-X 'main.Version=${version}'"
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for MacOS amd64! Aborting the script execution...'
     exit 1
 fi
 
 echo "Building binary ${version} for MacOS arm64..."
-env GOOS=darwin GOARCH=arm64 go build -o dist/darwin-arm64/ -ldflags="-X 'main.Version=${version}'"
+env GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o dist/darwin-arm64/ -ldflags="-X 'main.Version=${version}'"
 if [ $? -ne 0 ]; then
     echo 'An error has occurred building binary for MacOS arm64! Aborting the script execution...'
     exit 1
