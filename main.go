739f751350b9921165c487068e77a62d62fc1e80
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2023-03-21 15:09:07 +0100

    refactor(modules): extract zip facilities in separate module

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"archive/zip"
 	"context"
 	_ "embed"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -21,6 +18,8 @@ import (
 	"github.com/moby/term"
 	cp "github.com/otiai10/copy"
 	t "golang.org/x/term"
+
+	"github.com/bonitasoft/bonita-application-packager/zip"
 )
 
 const (
@@ -162,9 +161,9 @@ func buildTomcatBundle() {
 	bundleNameAndPath := filepath.Base(bundleAbsolutePath)                      // just the name of the zip file without the path
 	bundleName := bundleNameAndPath[0:strings.Index(bundleNameAndPath, ".zip")] // just the name without '.zip'
 	fmt.Printf("Unpacking Bonita Tomcat bundle %s.zip\n", bundleName)
-	unzipFile(bundleAbsolutePath, "output")
+	zip.UnzipFile(bundleAbsolutePath, "output")
 	fmt.Println("Unpacking Bonita WAR file")
-	unzipFile(filepath.Join("output", bundleName, "server", "webapps", "bonita.war"), filepath.Join("output", bundleName, "server", "webapps", "bonita"))
+	zip.UnzipFile(filepath.Join("output", bundleName, "server", "webapps", "bonita.war"), filepath.Join("output", bundleName, "server", "webapps", "bonita"))
 	if *verbose {
 		fmt.Println("Removing unpacked Bonita WAR file")
 	}
@@ -178,7 +177,7 @@ func buildTomcatBundle() {
 		copyResourceToCustomAppFolder(bundleName, *configurationFile)
 	}
 	fmt.Println("Re-packing Bonita bundle containing your application")
-	err := zipDirectory(filepath.Join("output", bundleName+"-application.zip"), filepath.Join("output", bundleName), bundleName)
+	err := zip.ZipDirectory(filepath.Join("output", bundleName+"-application.zip"), filepath.Join("output", bundleName), bundleName)
 	if err != nil {
 		panic(err)
 	}
@@ -372,129 +371,11 @@ func buildCustomDockerImage(ctx context.Context, dockerClient *client.Client, do
 	return nil
 }
 
-func unzipFile(zipFile string, outputDir string) {
-	archive, err := zip.OpenReader(zipFile)
-	if err != nil {
-		panic(err)
-	}
-	defer archive.Close()
-
-	for _, f := range archive.File {
-		filePath := filepath.Join(outputDir, f.Name)
-
-		if !strings.HasPrefix(filePath, filepath.Clean(outputDir)+string(os.PathSeparator)) {
-			fmt.Println("invalid file path")
-			return
-		}
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(filePath, os.ModePerm)
-			continue
-		}
-
-		if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-			panic(err)
-		}
-
-		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			panic(err)
-		}
-
-		fileInArchive, err := f.Open()
-		if err != nil {
-			panic(err)
-		}
-
-		if _, err := io.Copy(dstFile, fileInArchive); err != nil {
-			panic(err)
-		}
-
-		dstFile.Close()
-		fileInArchive.Close()
-	}
-}
-
 func Exists(filePath string) bool {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		return false
 	}
 	return true
-}
-
-func zipDirectory(zipFilename string, baseDir string, baseInZip string) error {
-	outFile, err := os.Create(zipFilename)
-	if err != nil {
-		return err
-	}
-
-	w := zip.NewWriter(outFile)
-
-	if err := addFilesToZip(w, baseDir, baseInZip); err != nil {
-		_ = outFile.Close()
-		return err
-	}
-
-	if err := w.Close(); err != nil {
-		_ = outFile.Close()
-		return errors.New("Warning: closing zipfile writer failed: " + err.Error())
-	}
-
-	if err := outFile.Close(); err != nil {
-		return errors.New("Warning: closing zipfile failed: " + err.Error())
-	}
-
-	return nil
-}
-
-func addFilesToZip(w *zip.Writer, basePath, baseInZip string) error {
-	files, err := ioutil.ReadDir(basePath)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		fullfilepath := filepath.Join(basePath, file.Name())
-		if _, err := os.Stat(fullfilepath); os.IsNotExist(err) {
-			// ensure the file exists. For example a symlink pointing to a non-existing location might be listed but not actually exist
-			continue
-		}
-
-		if file.Mode()&os.ModeSymlink != 0 {
-			// ignore symlinks alltogether
-			continue
-		}
-
-		if file.IsDir() {
-			// create dir first
-			path := filepath.Join(baseInZip, file.Name())
-			_, err := w.Create(path + "/")
-			if err != nil {
-				return err
-			}
-			// then add files inside it
-			if err := addFilesToZip(w, fullfilepath, baseInZip+"/"+file.Name()); err != nil {
-				return err
-			}
-		} else if file.Mode().IsRegular() {
-			dat, err := ioutil.ReadFile(fullfilepath)
-			if err != nil {
-				return err
-			}
-			fh := &zip.FileHeader{Name: baseInZip + "/" + file.Name()}
-			fh.SetMode(file.Mode())
-			f, err := w.CreateHeader(fh)
-			if err != nil {
-				return err
-			}
-			_, err = f.Write(dat)
-			if err != nil {
-				return err
-			}
-		} else {
-			// we ignore non-regular files because they are scary
-		}
-	}
-	return nil
 }
 
 func printMsgIfVerbose[T any](message string, o T) T {
